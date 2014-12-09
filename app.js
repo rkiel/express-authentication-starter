133a8fc0ef2c8df803067cff8ddccc5a53352bc1
rkiel <cygnus@pobox.com>
2014-12-08 23:12:16 -0500

    basic content in shell

---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require('express');
+
+var app = express();
+
+app.set('view engine', 'jade');
+
+app.get('/', function(req,res) {
+  res.render('index.jade');
+});
+app.get('/register', function(req,res) {
+  res.render('register.jade');
+});
+app.get('/login', function(req,res) {
+  res.render('login.jade');
+});
+app.get('/dashboard', function(req,res) {
+  res.render('dashboard.jade');
+});
+
+app.listen(3000);
