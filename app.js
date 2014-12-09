feda1d52ee68d1cdfb0651a229a712723641afc7
rkiel <cygnus@pobox.com>
2014-12-09 18:49:07 -0500

    added body-parser

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
-var express = require('express');
+var express    = require('express'),
+    bodyParser = require('body-parser');
 
 var app = express();
 
 app.set('view engine', 'jade');
 
+app.use(bodyParser.urlencoded({ 'extended' : true }));
+
 app.get('/', function(req,res) {
   res.render('index.jade');
 });
+
+app.post('/register', function(req,res) {
+  res.json(req.body);
+});
+
 app.get('/register', function(req,res) {
   res.render('register.jade');
 });
